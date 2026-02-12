603470c423f61e99951d1c06f398d241f11f262b
trisha <21trisha2005@gmail.com>
2026-02-12 17:11:34 +0530

    Time: 6 ms (17.31%) | Memory: 8.6 MB (65.75%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int original = x;
+        int rev = 0;
+        while(x>0)
+        {
+            int digit = x%10;
+            if(rev>INT_MAX/10 || rev<INT_MIN/10)
+            {
+                return 0;
+            }
+            rev = (rev*10)+digit;
+            x=x/10;
+        }
+        return original == rev;
+    }
+};
